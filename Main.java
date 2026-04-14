dbbff67949b64e2344704cee2a88bcddcf7764d6
zilitye <l.ztye0039@gmail.com>
2026-04-14 15:20:48 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,14 +1,28 @@
 import com.ex.calculate.MoneyCalculatorBean;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         MoneyCalculatorBean bean = new MoneyCalculatorBean();
-        bean.addIncome(100);
-        bean.addIncome(200);
-        bean.addExpense(50);
-        bean.addExpense(30);
+
+        System.out.print("Enter number of incomes: ");
+        int incomeCount = sc.nextInt();
+        for (int i = 0; i < incomeCount; i++) {
+            System.out.print("Enter income " + (i + 1) + ": ");
+            bean.addIncome(sc.nextInt());
+        }
+
+        System.out.print("Enter number of expenses: ");
+        int expenseCount = sc.nextInt();
+        for (int i = 0; i < expenseCount; i++) {
+            System.out.print("Enter expense " + (i + 1) + ": ");
+            bean.addExpense(sc.nextInt());
+        }
 
         System.out.println("Total Income: " + bean.getTotalIncome());
         System.out.println("Total Expenses: " + bean.getTotalExpenses());
+
+        sc.close();
     }
 }
