8837f7a6ca90a081da0da4e4d232d8c429286eaf
zilitye <l.ztye0039@gmail.com>
2026-04-16 04:02:41 +0800

    springboot

---
+++ src/test/java/com/ex/calculate/MoneyCalculatorBean/MoneyCalculatorBeanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ex.calculate.MoneyCalculatorBean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyCalculatorBeanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
