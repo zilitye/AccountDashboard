45e63d35d98af801e0c8b9e45a381d65a07fcba0
zilitye <l.ztye0039@gmail.com>
2026-04-15 11:27:35 +0800

    mvn

--- moneycalculatorbean/src/main/java/module-info.java
+++ moneycalculatorbean/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.ex.calculator {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.base;
 
     opens com.ex.calculator to javafx.fxml;
     exports com.ex.calculator;
